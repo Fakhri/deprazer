4f803ac4adee0bcff8296585cefa1caf2b93aa42
fakhri <fakhri.abdr@gmail.com>
2019-04-27 15:32:45 +0700

    Add sample us map

--- backend/application.py
+++ backend/application.py
@@ -1,12 +1,13 @@
 import os
 from flask import Flask, render_template
 
-template_dir = os.path.abspath('./frontend')
-application = Flask(__name__, template_folder=template_dir)
+template_dir = os.path.abspath('./frontend/template')
+static_dir = os.path.abspath('./frontend/static')
+application = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
 
 @application.route("/")
 def home():
-  return render_template('home.html')
+  return render_template('index.html')
  
 if __name__ == "__main__":
     application.run(debug=True)
